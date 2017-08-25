0f88d9aa9c57d370aa1dc6c0a11af97f358236ee
makubexd <makubex1719@gmail.com>
2017-08-25 00:04:36 -0500

    is lacking

--- Codility.Exams/Program.cs
+++ Codility.Exams/Program.cs
@@ -22,8 +22,13 @@ namespace Codility.Exams
             var methodFrogRiverOne = FrogRiverOne(5, new[] { 1, 3, 1, 4, 2, 3, 5, 4 }); // Lesson 04 - Counting Elements
             var methodMaxCounters = MaxCounters(5, new[] {3, 4, 4, 6, 1, 4, 4}); // Lesson 04 - Counting Elements
 
-            Console.WriteLine(string.Join(",", methodMaxCounters));
-            //Console.WriteLine(methodFrogRiverOne);
+            var methodPassingCars = PassingCars(new[] {0, 1, 0, 1, 1}); //Lesson 05 - Prefix Sums
+            var methodCountDiv = CountDiv(6, 11, 2); //Lesson 05 - Prefix Sums
+            var methodMinAvgTwoSlice = MinAvgTwoSlice(new[] {4, 2, 2, 5, 1, 5, 8}); //Lesson 05 - Prefix Sums
+            var methodGenomicRangeQuery = GenomicRangeQuery("CAGCCTA", new[] {2, 5, 0}, new[] {4, 5, 6}); //Lesson 05 - Prefix Sums
+
+            Console.WriteLine(string.Join(",", methodGenomicRangeQuery));
+            //Console.WriteLine(methodMinAvgTwoSlice);
             //Console.WriteLine(stopWatch.Elapsed.Milliseconds.ToString("D5"));
             Console.ReadLine();
         }
@@ -606,5 +611,256 @@ namespace Codility.Exams
             }
             return B;
         }
+
+        /// <summary>
+        /// Lesson 05 - Prefix Sums
+        /// A non-empty zero-indexed array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+        ///
+        ///Array A contains only 0s and/or 1s:
+        ///
+        ///0 represents a car traveling east,
+        ///1 represents a car traveling west.
+        ///The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P &lt; Q &lt; N, is passing when P is traveling to the east and Q is traveling to the west.
+        ///
+        ///For example, consider array A such that:
+        ///
+        ///  A[0] = 0
+        ///  A[1] = 1
+        ///  A[2] = 0
+        ///  A[3] = 1
+        ///  A[4] = 1
+        ///We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+        ///
+        ///Write a function:
+        ///
+        ///class Solution { public int solution(int[] A); }
+        ///that, given a non-empty zero-indexed array A of N integers, returns the number of pairs of passing cars.
+        ///
+        ///The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
+        ///
+        ///For example, given:
+        ///
+        ///  A[0] = 0
+        ///  A[1] = 1
+        ///  A[2] = 0
+        ///  A[3] = 1
+        ///  A[4] = 1
+        ///the function should return 5, as explained above.
+        ///
+        ///Assume that:
+        ///
+        ///N is an integer within the range [1..100,000];
+        ///each element of array A is an integer that can have one of the following values: 0, 1.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int PassingCars(int[] A)
+        {
+            long east = 0;
+            long passingCars = 0;
+            for (var i = 0; (i < A.Length); i++) {
+                if (A[i] == 0) {
+                    east++;
+                }
+                else {
+                    passingCars += east;
+                }
+            
+            }
+            return passingCars > 1000000000 ? -1 : (int)passingCars;
+        }
+
+        /// <summary>
+        /// Lesson 05 - Prefix Sum
+        /// Write a function:
+        ///
+        ///class Solution { public int solution(int A, int B, int K); }
+        ///that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+        ///
+        ///{ i : A ≤ i ≤ B, i mod K = 0 }
+        ///For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+        ///
+        ///Assume that:
+        ///
+        ///A and B are integers within the range [0..2,000,000,000];
+        ///K is an integer within the range [1..2,000,000,000];
+        ///A ≤ B.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static int CountDiv(int A, int B, int K)
+        {
+            return B / K - A / K + (A % K == 0 ? 1 : 0);
+        }
+        
+        /// <summary>
+        /// Lesson 05 - Prefix Sum
+        /// A non-empty zero-indexed array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P &lt; Q &lt; N, is called a slice of array A (notice that the slice contains at least two elements). The average of a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by the length of the slice. To be precise, the average equals (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
+        ///
+        ///For example, array A such that:
+        ///
+        ///    A[0] = 4
+        ///    A[1] = 2
+        ///    A[2] = 2
+        ///    A[3] = 5
+        ///    A[4] = 1
+        ///    A[5] = 5
+        ///    A[6] = 8
+        ///contains the following example slices:
+        ///
+        ///slice (1, 2), whose average is (2 + 2) / 2 = 2;
+        ///slice (3, 4), whose average is (5 + 1) / 2 = 3;
+        ///slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
+        ///The goal is to find the starting position of a slice whose average is minimal.
+        ///
+        ///Write a function:
+        ///
+        ///class Solution { public int solution(int[] A); }
+        ///that, given a non-empty zero-indexed array A consisting of N integers, returns the starting position of the slice with the minimal average. If there is more than one slice with a minimal average, you should return the smallest starting position of such a slice.
+        ///
+        ///For example, given array A such that:
+        ///
+        ///    A[0] = 4
+        ///    A[1] = 2
+        ///    A[2] = 2
+        ///    A[3] = 5
+        ///    A[4] = 1
+        ///    A[5] = 5
+        ///    A[6] = 8
+        ///the function should return 1, as explained above.
+        ///
+        ///Assume that:
+        ///
+        ///N is an integer within the range [2..100,000];
+        ///each element of array A is an integer within the range [−10,000..10,000].
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int MinAvgTwoSlice(int[] A)
+        {
+            var result = 0;
+            if (A.Length == 1 || A.Length == 2) return result;
+
+            var minAvg = Double.MaxValue;
+            for (var i = 0; i < A.Length - 2; i++) {
+                var currAvg = Math.Min((A[i] + A[i + 1]) / 2d, (A[i] + A[i + 1] + A[i + 2]) / 3d);
+                if (!(currAvg < minAvg)) continue;
+                minAvg = currAvg;
+                result = i;
+            }
+            return (A[A.Length - 2] + A[A.Length - 1]) / 2d < minAvg ? A.Length - 2 : result;
+        }
+        
+        /// <summary>
+        /// Lesson 05 - Prefix Sum
+        /// A DNA sequence can be represented as a string consisting of the letters A, C, G and T, which correspond to the types of successive nucleotides in the sequence. Each nucleotide has an impact factor, which is an integer. Nucleotides of types A, C, G and T have impact factors of 1, 2, 3 and 4, respectively. You are going to answer several queries of the form: What is the minimal impact factor of nucleotides contained in a particular part of the given DNA sequence?
+        ///
+        ///The DNA sequence is given as a non-empty string S = S[0]S[1]...S[N-1] consisting of N characters. There are M queries, which are given in non-empty arrays P and Q, each consisting of M integers. The K-th query (0 ≤ K &lt; M) requires you to find the minimal impact factor of nucleotides contained in the DNA sequence between positions P[K] and Q[K] (inclusive).
+        ///
+        ///For example, consider string S = CAGCCTA and arrays P, Q such that:
+        ///
+        ///    P[0] = 2    Q[0] = 4
+        ///    P[1] = 5    Q[1] = 5
+        ///    P[2] = 0    Q[2] = 6
+        ///The answers to these M = 3 queries are as follows:
+        ///
+        ///The part of the DNA between positions 2 and 4 contains nucleotides G and C (twice), whose impact factors are 3 and 2 respectively, so the answer is 2.
+        ///The part between positions 5 and 5 contains a single nucleotide T, whose impact factor is 4, so the answer is 4.
+        ///The part between positions 0 and 6 (the whole string) contains all nucleotides, in particular nucleotide A whose impact factor is 1, so the answer is 1.
+        ///Write a function:
+        ///
+        ///class Solution { public int[] solution(string S, int[] P, int[] Q); }
+        ///that, given a non-empty zero-indexed string S consisting of N characters and two non-empty zero-indexed arrays P and Q consisting of M integers, returns an array consisting of M integers specifying the consecutive answers to all queries.
+        ///
+        ///The sequence should be returned as:
+        ///
+        ///a Results structure (in C), or
+        ///a vector of integers (in C++), or
+        ///a Results record (in Pascal), or
+        ///an array of integers (in any other programming language).
+        ///For example, given the string S = CAGCCTA and arrays P, Q such that:
+        ///
+        ///    P[0] = 2    Q[0] = 4
+        ///    P[1] = 5    Q[1] = 5
+        ///    P[2] = 0    Q[2] = 6
+        ///the function should return the values [2, 4, 1], as explained above.
+        ///
+        ///Assume that:
+        ///
+        ///N is an integer within the range [1..100,000];
+        ///M is an integer within the range [1..50,000];
+        ///each element of arrays P, Q is an integer within the range [0..N − 1];
+        ///P[K] ≤ Q[K], where 0 ≤ K &lt; M;
+        ///string S consists only of upper-case English letters A, C, G, T.
+        /// </summary>
+        /// <param name="S"></param>
+        /// <param name="P"></param>
+        /// <param name="Q"></param>
+        /// <returns></returns>
+        public static int[] GenomicRangeQuery(string S, int[] P, int[] Q)
+        {
+            int aLen = S.Length;
+            char[] sAsCharArray = S
+                .Replace('A', '1')
+                .Replace('C', '2')
+                .Replace('G', '3')
+                .Replace('T', '4')
+                .ToCharArray();
+
+            var A = new int[aLen];
+            for (var i = 0; i < aLen; i++)
+            {
+                A[i] = (int)char.GetNumericValue(sAsCharArray[i]);
+            }
+
+            var M = P.Length;
+            var result = new int[M];
+            var F = new int[aLen + 1, 4];
+            int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+
+            for (var i = 0; i < aLen; i++)
+            {
+                switch(A[i])
+                {
+                    case 1:
+                        sum1 += 1;
+                        break;
+                    case 2:
+                        sum2 += 1;
+                        break;
+                    case 3:
+                        sum3 += 1;
+                        break;
+                    case 4:
+                        sum4 += 1;
+                        break;
+                }
+
+                F[i + 1, 0] = sum1;
+                F[i + 1, 1] = sum2;
+                F[i + 1, 2] = sum3;
+                F[i + 1, 3] = sum4;
+            }
+
+            for (var i = 0; i < M; i++)
+            {
+                var rangeLow = P[i];
+                var rangeHigh = Q[i];
+
+                for (var j = 0; j < 4; j++ )
+                {
+                    if (F[rangeHigh + 1, j] - F[rangeLow, j] <= 0) continue;
+                    result[i] = j + 1;
+                    break;
+                }
+            }
+
+            return result;
+
+        }
+
+
     }
 }
